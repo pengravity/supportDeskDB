0a569cda06255e1419cbaeaf38ea5dba618292fc
Andy <alipinski@mail.com>
2022-10-02 15:03:27 +0200

    setting basic routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcone to the support desk API' });
+});
+
+//Routes
+app.use('/api/users', require(`./routes/userRoutes`));
+
+app.listen(PORT, () => console.log(`Sever started on port ${PORT}`));
