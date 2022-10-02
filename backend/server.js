e96079971ad26198a0bebd63b7277b1083b02518
Andy <alipinski@mail.com>
2022-10-02 15:55:41 +0200

    simple validation, asyncHandler, errorHandler (json instead of html)

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 5000;
+
 const app = express();
+
+app.use(express());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcone to the support desk API' });
@@ -9,5 +14,7 @@ app.get('/', (req, res) => {
 
 //Routes
 app.use('/api/users', require(`./routes/userRoutes`));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Sever started on port ${PORT}`));
